4cce9b531776cff9f259640292dd1296059854dc
Donmark2k <chukwuemekavp@gmail.com>
2023-04-10 16:15:50 +0100

    Define the constructor

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+    attr_reader :id
+    attr_accessor :name, :age
+
+    def initialize (age, name = 'unknown', parent_permission: true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+
+
+
+
+
+
+
+
+end
