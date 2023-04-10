bec46f8782286b2d2e404c32ee6e967c3ec862c4
Donmark2k <chukwuemekavp@gmail.com>
2023-04-10 18:15:11 +0100

    Update readme

--- student.rb
+++ student.rb
@@ -1,13 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-    attr_accessor :classroom
-    def initialize(name = 'unknown', age, parent_permission: true, classroom)
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
-    
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  attr_accessor :classroom
+
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
