a32e72316c0bda2ab6964388f9e696321d1759bb
Donmark2k <chukwuemekavp@gmail.com>
2023-04-12 11:30:03 +0100

    Update the Rental class

--- rental.rb
+++ rental.rb
@@ -1,8 +1,16 @@
+require_relative 'book'
+require_relative 'person'
+
 class Rental 
     attr_accessor :date
+    attr_reader :book, :person
 
-    def initialize(date)
+    def initialize(date, book, person)
         @date = date
+        @person = person
+        @book = book
+        book.rentals << self
+        person.rentals << self
     end
-    
+
 end
