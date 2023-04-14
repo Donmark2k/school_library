62840b4f640ebae06a39f48043efdcd0b7695785
Donmark2k <chukwuemekavp@gmail.com>
2023-04-14 10:05:02 +0100

    Convert the input to integer

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require_relative 'app'
 def main
-  puts 'Welcome to School Library OOP App!'
+    app = App.new
+
+  puts 'Welcome to School Library '
   puts '----------------------------------'
-  app = App.new
   loop do
-    puts 'Please choose an option by entering a number:'
+    puts 'Please choose an option by entering a number from 1 to 7:'
     puts '1 - List all books'
     puts '2 - List all people'
     puts '3 - Create a person'
@@ -12,22 +13,22 @@ def main
     puts '5 - Create a rental'
     puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
-    option = gets.chomp
+    option = gets.chomp.to_i
     case option
-    when '1'
+    when 1
       app.list_books
-    when '2'
+    when 2
       app.list_people
-    when '3'
+    when 3
       app.create_person
-    when '4'
+    when 4
       app.create_book
-    when '5'
+    when 5
       app.create_rental
-    when '6'
+    when 6
       app.list_rentals_for_person_id
     end
-    break if option == '7'
+    break if option == 7
   end
   puts 'Thank you for using this app!'
 end
