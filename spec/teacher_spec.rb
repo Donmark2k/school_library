6629b2be2a782504c9c832ddb2ac0f7106846b45
Donmark2k <chukwuemekavp@gmail.com>
2023-04-24 07:25:36 +0100

    Teacher test is passing

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,14 +2,14 @@ require './teacher'
 
 describe Teacher do
   context 'create a teacher' do
-    teacher = Teacher.new(33, 'Ruby', 'John')
+    teacher = Teacher.new( 'Ruby', 33,'John')
 
     it 'check the age' do
       expect(teacher.age).to eq 33
     end
 
     it 'check the name' do
-      expect(teacher.name).to eq 'John'
+      expect(teacher.name).to eq 'Ruby'
     end
   end
 end
