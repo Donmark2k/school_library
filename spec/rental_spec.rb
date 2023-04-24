f9e5c625ad801d7bd4baf500a4fc8f91294ac1de
Donmark2k <chukwuemekavp@gmail.com>
2023-04-24 07:27:06 +0100

    Rental test is passing

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -5,7 +5,7 @@ require './student'
 describe Rental do
   context 'create a rental' do
     book = Book.new 'Purple_Hibiscus', 'Chiamanda'
-    student = Student.new(48, 'Chukwuemeka', 'ENG121', parent_permission: true)
+    student = Student.new('Chukwuemeka', 48,  'ENG121', parent_permission: true)
     rental = Rental.new( id,'01/04/2023', book, student)
 
     it 'check the date' do
@@ -20,13 +20,13 @@ describe Rental do
       expect(book.author).to eq 'Chiamanda'
     end
 
-    # it 'show student name' do
-    #   expect(student.name).to eq 'Chukwuemeka'
-    # end
+    it 'show student name' do
+      expect(student.name).to eq 'Chukwuemeka'
+    end
 
-    # it 'show student age' do
-    #   expect(student.age).to eq 48
-    # end
+    it 'show student age' do
+      expect(student.age).to eq 48
+    end
 
     it 'show classroom' do
       expect(student.classroom).to eq 'ENG121'
