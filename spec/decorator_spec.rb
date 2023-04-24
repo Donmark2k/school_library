a4adccb60e73eb7721ee72b23523935f2c8a5ca9
Donmark2k <chukwuemekavp@gmail.com>
2023-04-24 07:30:46 +0100

    Decorate test is now passing

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,35 +1,35 @@
-# require './decorator'
-# require './person'
-# require './nameable'
+require './decorator'
+require './person'
+require './nameable'
 
-# describe Decorator do
-#   before :each do
-#     @person = Person.new(27, 'chimamandaadiche')
-#     @person2 = Person.new(27, 'John')
-#   end
+describe Decorator do
+  before :each do
+    @person = Person.new('chimamandaadiche', 27)
+    @person2 = Person.new( 'John', 27)
+  end
 
-#   context 'CapitalizeDecorator' do
-#     it 'capitalizes the name' do
-#       capitalize = CapitalizeDecorator.new(@person)
-#       expect(capitalize.correct_name).to eq('Chimamandaadiche')
-#     end
-#     it 'capitalizes the name' do
-#       capitalize = CapitalizeDecorator.new(@person2)
-#       expect(capitalize.correct_name).to eq('John')
-#     end
-#   end
+  context 'CapitalizeDecorator' do
+    it 'capitalizes the name' do
+      capitalize = CapitalizeDecorator.new(@person)
+      expect(capitalize.correct_name).to eq('Chimamandaadiche')
+    end
+    it 'capitalizes the name' do
+      capitalize = CapitalizeDecorator.new(@person2)
+      expect(capitalize.correct_name).to eq('John')
+    end
+  end
 
-#   context 'TrimmerDecorator' do
-#     it 'trims name with more than 10 characters' do
-#       trimmer = TrimmerDecorator.new(@person)
-#       expect(trimmer.correct_name).to eq('chimamanda')
-#       expect(trimmer.correct_name.length).to be <= 10
-#     end
+  context 'TrimmerDecorator' do
+    it 'trims name with more than 10 characters' do
+      trimmer = TrimmerDecorator.new(@person)
+      expect(trimmer.correct_name).to eq('chimamanda')
+      expect(trimmer.correct_name.length).to be <= 10
+    end
 
-#     it 'trims name with more than 10 characters' do
-#       trimmer = TrimmerDecorator.new(@person2)
-#       expect(trimmer.correct_name).to eq('John')
-#       expect(trimmer.correct_name.length).to be <= 10
-#     end
-#   end
-# end
+    it 'trims name with more than 10 characters' do
+      trimmer = TrimmerDecorator.new(@person2)
+      expect(trimmer.correct_name).to eq('John')
+      expect(trimmer.correct_name.length).to be <= 10
+    end
+  end
+end
