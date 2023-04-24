797d05b2bd204f7fae3e6edab20615f665cb5eaf
Donmark2k <chukwuemekavp@gmail.com>
2023-04-24 07:33:55 +0100

    Person class test is now passing

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,31 +1,30 @@
-# require './person'
-# describe Person do
-#   context 'create a person' do
-#     person = Person.new(27, 'Microverse')
+require './person'
+describe Person do
+  context 'create a person' do
+    person = Person.new( 'Microverse', 27)
 
-#     it ' show age' do
-#       expect(person.age).to eq 27
-#       expect(person.age).to be_instance_of Fixnum 
+    it ' show age' do
+      expect(person.age).to eq 27
 
-#     end
+    end
 
-#     it 'show name' do
-#       expect(person.name).to eq 'Microverse'
-#     end
+    it 'show name' do
+      expect(person.name).to eq 'Microverse'
+    end
 
-#     it 'when the person does not have parent permission' do
-#       person = Person.new(16, 'James', parent_permission: false)
-#       expect(person.can_use_services?).to eq(false)
-#     end
+    it 'when the person does not have parent permission' do
+      person = Person.new( 'James', 16, parent_permission: false)
+      expect(person.can_use_services?).to eq(false)
+    end
 
-#     it 'when the person does not have parent permission but returns true if the person is above 18' do
-#       person = Person.new(21, 'James', parent_permission: false)
-#       expect(person.can_use_services?).to eq(true)
-#     end
+    it 'when the person does not have parent permission but returns true if the person is above 18' do
+      person = Person.new( 'James', 21, parent_permission: false)
+      expect(person.can_use_services?).to eq(true)
+    end
 
-#     it 'when the person  have parent permission' do
-#       person = Person.new(26, 'James', parent_permission: true)
-#       expect(person.can_use_services?).to eq(true)
-#     end
-#   end
-# end
+    it 'when the person  have parent permission' do
+      person = Person.new('James', 26, parent_permission: true)
+      expect(person.can_use_services?).to eq(true)
+    end
+  end
+end
