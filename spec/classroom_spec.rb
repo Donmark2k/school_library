1f247f57f4c3977b09dc455afdc5b9d6caa579f7
Donmark2k <chukwuemekavp@gmail.com>
2023-04-24 07:15:51 +0100

    Classroom test is passing

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,11 +1,11 @@
-# require './classroom'
+require './classroom'
 
-# describe Classroom do
-#   context 'create a classroom' do
-#     classroom = Classroom.new('mob_programming')
+describe Classroom do
+  context 'create a classroom' do
+    classroom = Classroom.new('mob_programming')
 
-#     it 'show classroom' do
-#       expect(classroom.label).to eq 'mob_programming'
-#     end
-#   end
-# end
+    it 'show classroom' do
+      expect(classroom.label).to eq 'mob_programming'
+    end
+  end
+end
